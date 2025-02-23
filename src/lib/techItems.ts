2eab77627e8acfc88be8ccd2082c23d26b557238
Phil Vishnevsky <gadgetsphilop36@gmail.com>
2025-02-23 13:10:54 -0500

    move techitems to own lib file

---
+++ src/lib/techItems.ts
@@ -0,0 +1,54 @@
+export const techItems = [
+  {
+    image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg',
+    link: 'https://www.typescriptlang.org/'
+  },
+  {
+    image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg',
+    link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+  },
+  {
+    image: 'https://raw.githubusercontent.com/devicons/devicon/ca28c779441053191ff11710fe24a9e6c23690d6/icons/tailwindcss/tailwindcss-original.svg',
+    link: 'https://tailwindcss.com/'
+  },
+  {
+    image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg',
+    link: 'https://nextjs.org/'
+  },
+  {
+    image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg',
+    link: 'https://nodejs.org/'
+  },
+  {
+    image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg',
+    link: 'https://reactjs.org/'
+  },
+  {
+    image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg',
+    link: 'https://git-scm.com/'
+  },
+  {
+    image: 'https://raw.githubusercontent.com/devicons/devicon/ca28c779441053191ff11710fe24a9e6c23690d6/icons/amazonwebservices/amazonwebservices-original-wordmark.svg',
+    link: 'https://aws.amazon.com/'
+  },
+  {
+    image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/unity/unity-original.svg',
+    link: 'https://unity.com/'
+  },
+  {
+    image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg',
+    link: 'https://isocpp.org/'
+  },
+  {
+    image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg',
+    link: 'https://docs.microsoft.com/en-us/dotnet/csharp/'
+  },
+  {
+    image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg',
+    link: 'https://www.python.org/'
+  },
+  {
+    image: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/jupyter/jupyter-original.svg',
+    link: 'https://jupyter.org/'
+  }
+];
